887b266641c93d53dc4459972709ef254c10376f
angadbasak <angadbasak.code@gmail.com>
2022-08-01 23:26:30 +0600

    String Added by Pranto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Bappy from "./components/faysal/Bappy";
+import Angad from "./components/Pranto/Angad";
 
 function App() {
  
   return <div className="App">
     <Bappy name="Bappy" />
+    <Angad/>
   </div>;
 }
 export default App;
