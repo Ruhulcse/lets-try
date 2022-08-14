89e4e409a292a2dd26e85075802055b6f2f532ea
Bappy98 <faysalahmed.cse.98@gmail.com>
2022-08-14 21:15:39 +0600

    img problem solve

--- src/components/faysal/Bappy.jsx
+++ src/components/faysal/Bappy.jsx
@@ -3,6 +3,7 @@ import {Title} from './styled/Title.styled';
 import {Content} from './styled/Content.styled'
 import {ImageContainer} from './styled/ImageContainer.styled'
 import {Info} from './styled/Info.styled'
+import myphoto from './img/IMG_20220731_120514.jpg';
 function Bappy () { 
     return <>
     <Main>
@@ -13,7 +14,7 @@ function Bappy () {
             </Title>
                <Content>
                 <ImageContainer>
-                    <img src="https://scontent.fdac116-1.fna.fbcdn.net/v/t1.6435-9/83402006_756103394881887_5341243134854561792_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGqwD54fNo40KTuamfCFmkm1PUgKe78T_DU9SAp7vxP8KmkbiSjj2dB4_FsxZEQ4BKKkUnXZUB3fdHWfv-7ZKRR&_nc_ohc=55xmftSOfwIAX_NkcjZ&tn=dgFT00LoRVYgRgrL&_nc_ht=scontent.fdac116-1.fna&oh=00_AT9GmbJlFFlfyONYVnLZ-ZTRtvNNxS63MefWfyMdz67pLw&oe=631E05D9" alt="555" />
+                    <img src={myphoto} alt='Faysal'/>
                 </ImageContainer>
                 <Info>
                     <p>Hi, I am bappy</p>
